6423c41f8c0efe79599e1004a5171a22ba61b910
Aldo Fregoso <aldomx@gmail.com>
2017-01-05 21:31:26 -0600

    Updated build options.

--- src/archutils/Win32/arch_setup.h
+++ src/archutils/Win32/arch_setup.h
@@ -50,17 +50,19 @@ typedef unsigned __int64 uint64_t;
 	static inline int64_t llabs( int64_t i ) { return i >= 0? i: -i; }
 #endif
 
-#pragma warning (disable : 4201) // nonstandard extension used : nameless struct/union (Windows headers do this)
-#pragma warning (disable : 4786) // turn off broken debugger warning
-#pragma warning (disable : 4512) // assignment operator could not be generated (so?)
-/* "unreferenced formal parameter"; we *want* that in many cases */
-#pragma warning (disable : 4100)
-/* "case 'aaa' is not a valid value for switch of enum 'bbb'
- * Actually, this is a valid warning, but we do it all over the
- * place, eg. with ScreenMessages.  Those should be fixed, but later. XXX */
-#pragma warning (disable : 4063)
-#pragma warning (disable : 4127)
-#pragma warning (disable : 4786) /* VC6: identifier was truncated to '255' characters in the debug information */
+#if defined(_MSC_VER)
+	#pragma warning (disable : 4201) // nonstandard extension used : nameless struct/union (Windows headers do this)
+	#pragma warning (disable : 4786) // turn off broken debugger warning
+	#pragma warning (disable : 4512) // assignment operator could not be generated (so?)
+	/* "unreferenced formal parameter"; we *want* that in many cases */
+	#pragma warning (disable : 4100)
+	/* "case 'aaa' is not a valid value for switch of enum 'bbb'
+	 * Actually, this is a valid warning, but we do it all over the
+	 * place, eg. with ScreenMessages.  Those should be fixed, but later. XXX */
+	#pragma warning (disable : 4063)
+	#pragma warning (disable : 4127)
+	#pragma warning (disable : 4786) /* VC6: identifier was truncated to '255' characters in the debug information */
+#endif
 
 #undef min
 #undef max
